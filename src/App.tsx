e1f8fd04865ac54eeedf4cc5117df7dbf79d8433
mina-gwak <mina-gwak@kakao.com>
2021-07-07 16:23:14 +0900

    Add library

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>PINCAR</h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
     </div>
   );
 }
